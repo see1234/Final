2fff46965bde63bb9048066cbb5796c49cf835eb
see1 <mr_see1@vk.com>
2022-09-06 09:49:00 +0300

    Добавление программы для дз

---
+++ csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write("\"" + array[i] + "\", ");
+         
+    }
+}
+Console.Write("\b\b]");
